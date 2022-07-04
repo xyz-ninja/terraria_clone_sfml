aaf58b8ab8c21bc2e5e5afa9e2d0ead54b3b20b9
Vladislav <boxofmatic@gmail.com>
2022-07-04 15:37:04 +0300

    oop entity stuff

---
+++ Entities/Entity.cs
@@ -0,0 +1,170 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TerrariaCloneV2.Entities
+{
+	abstract class Entity : Transformable, Drawable
+	{
+		protected RectangleShape rect;
+		protected Vector2f velocity;
+		protected Vector2f movement;
+
+		protected World world;
+		protected bool isFly = true;
+
+		protected Vector2f startPosition;
+
+		#region getters/setters
+
+		public int Direction {
+			set 
+			{	
+				int dir = value >= 0 ? 1 : -1;
+
+				Scale = new Vector2f(dir, 1);
+			}
+
+			get 
+			{
+				int dir = Scale.X >= 0 ? 1 : -1;
+
+				return dir;
+			}
+		}
+
+		#endregion
+
+		public Entity(World world) {
+			this.world = world;
+		}
+
+		public void Spawn(Vector2f startPosition) {
+
+			this.startPosition = startPosition;
+
+			Position = startPosition;
+			velocity = new Vector2f();
+		}
+
+		public void Update() {
+
+			UpdateEntity();
+			UpdatePhysics();
+
+			Position += movement + velocity;
+
+			if (Position.Y > Program.RenderWindow.Size.Y) {
+				OnKill();
+			}
+		}
+
+		private void UpdatePhysics() {
+
+			bool isFall = true;
+
+			//velocity += new Vector2f(0, 0.15f);
+			velocity.X *= 0.99f;
+
+			// гравитация
+			velocity.Y += 0.25f;
+
+			// проверяем коллизию с тайлом
+			Vector2f nextPosition = Position + velocity - rect.Origin;
+
+			FloatRect entityRect = new FloatRect(nextPosition, rect.Size);
+
+			// ищем тайл игрока
+
+			int pX = (int)((Position.X - rect.Origin.X + rect.Size.X / 2) / Tile.TILE_SIZE);
+			int pY = (int)((Position.Y + rect.Size.Y) / Tile.TILE_SIZE);
+
+			Tile tile = world.GetTile(pX, pY);
+
+			if (tile != null) {
+
+				FloatRect tileRect = new FloatRect(
+					tile.Position,
+					new Vector2f(Tile.TILE_SIZE, Tile.TILE_SIZE)
+				);
+
+				DebugRenderer.AddRectangle(tileRect, Color.Red);
+
+				isFall = entityRect.Intersects(tileRect) == false;
+				isFly = isFall;
+			}
+
+			if (!isFall) {
+				velocity.Y = 0;
+			}
+
+			UpdateWallsCollisions(entityRect, pX, pY);
+		}
+
+		private void UpdateWallsCollisions(FloatRect entityRect, int pX, int pY) {
+			Tile[] walls = new Tile[] {
+				world.GetTile(pX - 1, pY - 1),
+				world.GetTile(pX - 1, pY - 2),
+				world.GetTile(pX - 1, pY - 3),
+				world.GetTile(pX + 1, pY - 1),
+				world.GetTile(pX + 1, pY - 2),
+				world.GetTile(pX + 1, pY - 3),
+			};
+
+			foreach (Tile wallTile in walls) {
+				if (wallTile == null) {
+					continue;
+				}
+
+				FloatRect tileRect = new FloatRect(
+					wallTile.Position,
+					new Vector2f(Tile.TILE_SIZE, Tile.TILE_SIZE)
+				);
+
+				DebugRenderer.AddRectangle(tileRect, Color.Yellow);
+
+				if (entityRect.Intersects(tileRect)) {
+
+					Vector2f offset = new Vector2f(entityRect.Left - tileRect.Left, 0);
+					offset.X /= Math.Abs(offset.X);
+
+					float speed = Math.Abs(movement.X);
+
+					if (offset.X > 0) {
+
+						Position = new Vector2f((tileRect.Left + tileRect.Width) + entityRect.Width / 2, Position.Y);
+
+						movement.X = 0;
+						velocity.X = 0;
+
+					} else if (offset.X < 0) {
+
+						Position = new Vector2f(tileRect.Left - entityRect.Width / 2, Position.Y);
+
+						movement.X = 0;
+						velocity.X = 0;
+					}
+
+					OnWallCollided();
+				}
+			}
+		}
+
+		public void Draw(RenderTarget target, RenderStates states) {
+			states.Transform *= Transform;
+
+			target.Draw(rect, states);
+
+			DrawEntity(target, states);
+		}
+
+		public abstract void OnKill();
+		public abstract void OnWallCollided();
+		public abstract void UpdateEntity();
+		public abstract void DrawEntity(RenderTarget target, RenderStates states);
+	}
+}
--- Player.cs
+++ Player.cs
@@ -6,52 +6,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SFML.Window;
+using TerrariaCloneV2.Entities;
+using System.Diagnostics;
 
 namespace TerrariaCloneV2
 {
-	class Player : Transformable, Drawable
+	class Player : Entity
 	{
-		private World world;
-
-		private Vector2f rectSize;
-		private RectangleShape rect;
 		private RectangleShape rectDirection;
-
-		private Vector2f velocity;
-		private Vector2f movement; // вектор перемещения игрока
 
 		private float horizontalSpeed = 4f;
 		private float horizontalSpeedAcceleration = 0.2f;
 
-		private Vector2f startPosition;
-
-		#region getters/setters
-
-		public int Direction {
-			set {
-
-				int dir = value >= 0 ? 1 : -1;
-				
-				Scale = new Vector2f(dir, 1);
-			}
-
-			get {
-
-				int dir = Scale.X >= 0 ? 1 : -1;
-
-				return dir;
-			}
-		}
-
-		#endregion
-
-		public Player(World world) {
-
-			this.world = world;
-
-			rectSize = new Vector2f(Tile.TILE_SIZE * 1.5f, Tile.TILE_SIZE * 2.8f);
-			
-			rect = new RectangleShape(rectSize);
+		public Player(World world) : base(world)
+		{
+			rect = new RectangleShape(new Vector2f(Tile.TILE_SIZE * 1.5f, Tile.TILE_SIZE * 2.8f));
 			rect.Origin = new Vector2f(rect.Size.X / 2, 0);
 
 			rectDirection = new RectangleShape(new Vector2f(50, 3));
@@ -59,104 +28,23 @@ namespace TerrariaCloneV2
 			rectDirection.Position = new Vector2f(0, rect.Size.Y / 2 - 1);
 		}
 
-		public void Spawn(Vector2f startPosition) {
-
-			this.startPosition = startPosition;
-
-			Position = startPosition;
-			velocity = new Vector2f();
+		public override void OnKill() {
+			Spawn(this.startPosition);
 		}
 
-		public void Update() {
+		public override void OnWallCollided() {
 
+			Debug.Print("Collided");
+
+			//throw new NotImplementedException();
+		}
+
+		public override void UpdateEntity() {
 			UpdateMovement();
-			UpdatePhysics();
-
-			Position += movement + velocity;
-
-			if (Position.Y > Program.RenderWindow.Size.Y) {
-				Spawn(this.startPosition);
-			}
 		}
 
-		private void UpdatePhysics() {
-
-			bool isFall = true;
-
-			velocity += new Vector2f(0, 0.15f);
-
-			// проверяем коллизию с тайлом
-			Vector2f nextPosition = Position + velocity - rect.Origin;
-
-			FloatRect playerRect = new FloatRect(nextPosition, rect.Size);
-
-			// ищем тайл игрока
-
-			int pX = (int)((Position.X - rect.Origin.X + rect.Size.X / 2) / Tile.TILE_SIZE);
-			int pY = (int)((Position.Y + rect.Size.Y) / Tile.TILE_SIZE);
-
-			Tile tile = world.GetTile(pX, pY);
-
-			if (tile != null) {
-
-				FloatRect tileRect = new FloatRect(
-					tile.Position,
-					new Vector2f(Tile.TILE_SIZE, Tile.TILE_SIZE)
-				);
-
-				DebugRenderer.AddRectangle(tileRect, Color.Red);
-
-				isFall = !playerRect.Intersects(tileRect);
-			}
-
-			if (!isFall) {
-				velocity.Y = 0;
-			}
-
-			UpdateWallsCollisions(playerRect, pX, pY);
-		}
-
-		private void UpdateWallsCollisions(FloatRect playerRect, int pX, int pY) {
-			Tile[] walls = new Tile[] { 
-				world.GetTile(pX - 1, pY - 1),
-				world.GetTile(pX - 1, pY - 2),
-				world.GetTile(pX - 1, pY - 3),
-				world.GetTile(pX + 1, pY - 1),
-				world.GetTile(pX + 1, pY - 2),
-				world.GetTile(pX + 1, pY - 3),
-			};
-
-			foreach (Tile wallTile in walls) {
-				if (wallTile == null) {
-					continue;
-				}
-
-				FloatRect tileRect = new FloatRect(
-					wallTile.Position, 
-					new Vector2f(Tile.TILE_SIZE, Tile.TILE_SIZE)
-				);
-
-				DebugRenderer.AddRectangle(tileRect, Color.Yellow);
-
-				if (playerRect.Intersects(tileRect)) {
-
-					Vector2f offset = new Vector2f(playerRect.Left - tileRect.Left, 0);
-					offset.X /= Math.Abs(offset.X);
-
-					float speed = Math.Abs(movement.X);
-
-					if (offset.X > 0) {
-						
-						movement.X = (tileRect.Left + tileRect.Width) - playerRect.Left;
-						velocity.X = 0;
-
-					} else if (offset.X < 0) {
-
-						movement.X = tileRect.Left - (playerRect.Left + playerRect.Width);
-						velocity.X = 0;
-					}
-				}
-			}
+		public override void DrawEntity(RenderTarget target, RenderStates states) {
+			target.Draw(rectDirection, states);
 		}
 
 		private void UpdateMovement() {
@@ -198,14 +86,6 @@ namespace TerrariaCloneV2
 
 				movement.X = 0;
 			}
-		}
-
-		public void Draw(RenderTarget target, RenderStates states) {
-			
-			states.Transform *= Transform;
-
-			target.Draw(rect, states);
-			target.Draw(rectDirection, states);
 		}
 	}
 }
