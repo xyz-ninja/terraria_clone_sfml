24e8d6a8a5faa2bc827666989be7f24f4d5b6fe9
Vladislav <boxofmatic@gmail.com>
2022-07-04 15:46:47 +0300

    slime ai

--- Entities/NpcSlime.cs
+++ Entities/NpcSlime.cs
@@ -7,23 +7,27 @@ namespace TerrariaCloneV2.Entities
 	class NpcSlime : Entity
 	{
 		public NpcSlime(World world) : base(world) {
-			rect = new RectangleShape();
-		}
-
-		public override void DrawEntity(RenderTarget target, RenderStates states) {
-			throw new NotImplementedException();
+			rect = new RectangleShape(new Vector2f(Tile.TILE_SIZE * 1.5f, Tile.TILE_SIZE * 1f));
+			rect.Origin = new Vector2f(rect.Size.X / 2, 0);
+			rect.FillColor = Color.Green;
 		}
 
 		public override void OnKill() {
-			throw new NotImplementedException();
+			Spawn(this.startPosition);
 		}
 
 		public override void OnWallCollided() {
-			throw new NotImplementedException();
+			Direction *= -1;
 		}
 
 		public override void UpdateEntity() {
-			throw new NotImplementedException();
+			if (isFly == false) {
+				velocity = new Vector2f(Direction * 10, -7);
+			}
+		}
+
+		public override void DrawEntity(RenderTarget target, RenderStates states) {
+			//throw new NotImplementedException();
 		}
 	}
 }
--- Game.cs
+++ Game.cs
@@ -1,4 +1,5 @@
 ﻿using SFML.System;
+using TerrariaCloneV2.Entities;
 
 namespace TerrariaCloneV2
 {
@@ -6,6 +7,7 @@ namespace TerrariaCloneV2
 	{
 		private World world;
 		private Player player;
+		private NpcSlime npcSlime;
 
 		public Game() {
 			
@@ -17,18 +19,24 @@ namespace TerrariaCloneV2
 			player = new Player(world);
 			player.Spawn(new Vector2f(300, 150));
 
+			// создаём слизня
+			npcSlime = new NpcSlime(world);
+			npcSlime.Spawn(new Vector2f(500, 150));
+
 			DebugRenderer.Enabled = true;
 		}
 
 		public void Update() {
 			
 			player.Update();
+			npcSlime.Update();
 		}
 
 		public void Draw() {
 
 			Program.RenderWindow.Draw(world);
 			Program.RenderWindow.Draw(player);
+			Program.RenderWindow.Draw(npcSlime);
 
 			DebugRenderer.Draw(Program.RenderWindow);
 		}
